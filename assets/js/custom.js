2cb34616fba46c7e893afd916bec9b4a9abb5319
Alessandra <alessandra.manna89@gmail.com>
2015-05-09 18:44:43 +0200

    hidden social

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
     
+    var numSections = 4;
+    
     var x = ($(window).width() / 10) -5;
     var y = 0;
     var height = $(window).height() / 1.2; 
@@ -23,6 +25,9 @@ $(document).ready(function() {
     var line = paper.path('M0 0');
     
     var circles = new Array();
+    var colours = ['#fff59b', '#ff6f69', '#88d8b0', '#ffcc5c'];
+    var ids = ['about', 'resume', 'blog', 'contacts'];
+
     
     //g.append(line);
     
@@ -48,19 +53,19 @@ $(document).ready(function() {
     );
     
     function drawCircles(circles, i){
-        if(i < 4){
+        if(i < numSections){
             var circle = paper.path('M0 '+(height-100));
             //g.append(lines[i]);
             circle.toBack();
             circle.attr(
                 {
                     'stroke-width': 2,
-                    'class': 'pagess'
-                });
+                }).click(function(){
+                openPage(this, $(this));
+            })
             var incr = 20*i;
             var newheight = (height-101-incr);
             var end = (newheight.toString()).split(".")[0]+'.01';
-            var colours = ['#fff59b', '#ff6f69', '#88d8b0', '#ffcc5c'];
             circle.animate(
                 {
                     path: 'M'+(1+i)+' '+newheight+','+ 
@@ -73,12 +78,12 @@ $(document).ready(function() {
                 }
             );
             
-            $(circle.node).attr('class', 'page');
+            var c = $(circle.node);
+            c.attr('class', 'page');
+            c.attr('id', ids[i]);            
             
             var hoverIn = function() {
-                this.attr({
-                    "stroke": '#E3E3E3'
-                });
+                this.attr({"stroke": '#E3E3E3'});
             };
     
             var hoverOut = function() {
@@ -86,11 +91,17 @@ $(document).ready(function() {
             }
     
             circle.hover(hoverIn, hoverOut, circle, circle);
+            
         }
        
     };
 
    
+    function openPage(r, e){
+        var tfm = 'S2,0, 0,2,0';
+        var w = r.attr('width');
+        r.transform(tfm);
+    }
   
     
    /* function svgEl(tagName){
