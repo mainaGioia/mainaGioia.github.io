24a1ae8adc453acad06333c741bd4abbbb90b45f
mainaGioia <alessandra.manna89@gmail.com>
2017-02-13 23:27:55 +0100

    Work in progress

--- assets/js/map.js
+++ assets/js/map.js
@@ -65,12 +65,14 @@ var country;
                
   function get_xyz(d) {
   var bounds = path.bounds(d);
-      console.log("chi e path? ",d, " e bounds: ",bounds);
+      console.log("data: ", d);
+      console.log("bounds: ",bounds);
   var w_scale = (bounds[1][0] - bounds[0][0]) / width;
   var h_scale = (bounds[1][1] - bounds[0][1]) / height;
   var z = .96 / Math.max(w_scale, h_scale);
   var x = (bounds[1][0] + bounds[0][0]) / 2;
   var y = (bounds[1][1] + bounds[0][1]) / 2 + (height / z / 6);
+      console.log(x, " ", y, " ", z);
   return [x, y, z];
 }
 
@@ -88,7 +90,7 @@ function zoom(xyz) {
     console.log("ci sono");
   g.transition()
     .duration(750)
-    .attr("transform", "translate(" + projection.translate() + ")scale(" + 2 + ")translate(-" + xyz[0] + ",-" + xyz[1] + ")")
+    .attr("transform", "translate(" + projection.translate() + ")scale(" + xyz[2] + ")translate(-" + xyz[0] + ",-" + xyz[1] + ")")
     .selectAll(["#countries", "#states", "#cities"])
     .style("stroke-width", 1.0 / xyz[2] + "px")
     .selectAll(".city")
@@ -109,11 +111,25 @@ function pointClicked(d) {
   }
 
   if (d && country !== d) {
-    var xyz = get_xyz(d);
+    /*var xyz = get_xyz(d);*/
     console.log("xyz = ",xyz);
     /*country = d;*/
 
     if (d.properties.state  == 'Italy') {
+      var region = d3.json("/assets/data/regions.topo.json", function(error, reg){
+          g.append("g")
+          .attr("id", "region")
+          .selectAll(".region")
+          .data(topojson.feature(reg, reg.objects.regions).features)
+          .enter()
+          .append("path")
+          .attr("id", function(d) { return d.properties.description; })
+          .attr("class", "active")
+          .attr("d", path)
+          .style('stroke', '#fff59b')
+        
+          xyz = get_xyz(d3.select("#Campania"));
+      });
       d3.json("/assets/data/extplaces.topo.json", function(error, campania) {
         g.append("g")
           .attr("id", "comuni")
@@ -123,11 +139,10 @@ function pointClicked(d) {
           .append("path")
           .attr("id", function(d) { return d.id; })
           .attr("class", "active")
-          .attr("d", path)
+          .attr("d", path.pointRadius(5))
           .style('fill', '#fff59b')
           .on('mouseover', tip.show)
           .on('mouseout', tip.hide)
-
 
         zoom(xyz);
         g.selectAll("#" + d.id).style('display', 'none');
